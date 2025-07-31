06814b24d9e7f89e65e08507ec93eb202964ece2
Khalid <96749372+a0zhar@users.noreply.github.com>
2025-07-31 17:39:45 +0200

    Update ViewController.swift

--- I am Groot/ViewController.swift
+++ I am Groot/ViewController.swift
@@ -1,19 +1,46 @@
-//
-//  ViewController.swift
-//  I am Groot
-//
-//  Created by Hiro Protagonist on 11/27/22.
-//
-
+// ViewController.swift
 import UIKit
 
 class ViewController: UIViewController {
+    private let logView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.isEditable = false
+        return textView
+    }()
+
+    private let resultLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.text = "Running XPC PoC..."
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(logView)
+        NSLayoutConstraint.activate([
+            logView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            logView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            logView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            logView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
+        ])
+        view.addSubview(resultLabel)
+        NSLayoutConstraint.activate([
+            resultLabel.topAnchor.constraint(equalTo: logView.bottomAnchor, constant: 20),
+            resultLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            resultLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            resultLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+
+        DispatchQueue.global(qos: .userInitiated).async {
+            let result = String(cString: run_xpc_poc())
+            DispatchQueue.main.async {
+                self.logView.text = "PoC Log:\n\(result)"
+                self.resultLabel.text = result.contains("Failed") ? "PoC Failed" : "PoC Succeeded"
+            }
+        }
     }
-
-
 }
-
